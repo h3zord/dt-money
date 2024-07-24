35b6f52569f9ba7b599cc462d10e82bce18fbb60
Lucas <lucaschmain@gmail.com>
2024-07-24 15:58:49 -0300

    feat: installing radix ui dialog

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import * as Dialog from '@radix-ui/react-dialog'
 import logoApp from '../../assets/logo.svg'
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 
@@ -7,7 +8,21 @@ export function Header() {
       <HeaderContent>
         <img src={logoApp} alt="Logo da aplicação" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
